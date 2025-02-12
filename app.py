3784eb2c76118c63b135b9433e397a899ae6c98d
bhavesh2k <bhavesh.agarwal2k@mail.com>
2025-02-13 00:14:03 +0530

    Fix: gunicorn and PORT issue

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 import numpy as np
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -42,4 +43,5 @@ def predict():
 
 # Run Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
